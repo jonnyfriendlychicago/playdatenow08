7d15a5510c49ca600d658a89057fb0e73fc8ddfe
JonFriend <jonfriend@jonfriends-mbp.lan>
2022-10-09 19:32:41 -0500

    java project created; readMe and gitIgnore added

---
+++ src/main/java/com/f12s/playdatenow08/Playdatenow08Application.java
@@ -0,0 +1,13 @@
+package com.f12s.playdatenow08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Playdatenow08Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Playdatenow08Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/f12s/playdatenow08/Playdatenow08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f12s.playdatenow08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Playdatenow08ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
