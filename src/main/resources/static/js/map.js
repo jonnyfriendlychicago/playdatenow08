4204c0209b8e526c4e5ce69a282b1e2a3654996d
JonFriend <jonfriend@JonFriends-MacBook-Pro.local>
2022-10-27 14:25:35 -0500

    removed junk code

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,9 +1,9 @@
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
         center: {lat: 45.334120, lng:   -121.69868},
-        zoom: 12
+        zoom: 12,
     });
-};
+}
 
 // window.initMap = initMap();
 window.initMap = initMap;
